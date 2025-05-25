997f2a95a4ae63a833866c896990f45bd2845da3
Nolan Castaneda <ncastan25@gmail.com>
2025-05-24 22:40:23 -0400

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${
         isLoaded ? "opacity-100" : "opacity-0"
       } bg-black text-gray-100`}>
-        <Router>
+        <Router basename="/Portfolio">
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <Routes>
